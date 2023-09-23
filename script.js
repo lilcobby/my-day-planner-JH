ec6b748fd8c4df8797943774a8300b7febcf9b2a
Cob H <jshairfield13@gmail.com>
2023-09-23 17:02:46 -0400

    played around with some different time features, have a few functional ideas

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
 // Wrap all code that interacts with the DOM in a call to jQuery to ensure that
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
+// date.now or datejs?? 
+var date = Date.now();
+var now= new Date().getHours()
+var saveButtons = $('.btn');
+console.log(now);
 $(function () {
   // TODO: Add a listener for click events on the save button. This code should
   // use the id in the containing time-block as a key to save the user input in
